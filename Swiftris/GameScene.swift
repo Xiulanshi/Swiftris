34cc65f9979a783d52a3814da53188c30a176e09
Xiulan Shi <xiulanshi2013@gmail.com>
2016-01-21 14:02:49 -0500

    Added background

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -9,8 +9,24 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background") //background is the variable's name, Swift infers its type to be that of SKSpriteNode and the keyword let indicates that it can not be re-assigned.
+        background.position = CGPoint(x: 0, y: 0)   //(0, 0) in SpriteKit is the bottom-left corner
+        background.anchorPoint = CGPoint(x: 0, y: 1.0) //the top-left corner of the screen: (0, 1.0)
+        addChild(background)
+    }
        
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
 }
+
--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Configure the view.  //The as! operator is a forced downcast. The view object is of type SKView, but before downcasting, our code treated it like a basic UIView. Without downcasting, we are unable to access SKView methods and properties, such as presentScene(SKScene).
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the scene.
+        skView.presentScene(scene)
+        
     }
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
 }
+/*In GameViewController we've added a member variable, scene. Its declaration: var scene: GameScene! lets us know that it's a variable, its name is scene, its type is GameScene and it's a non-optional value which we will assign at some point.
+
+Swift typically enforces instantiation either in-line where you declare the variable or during the initializer, init…. To avoid this rule, we've added an ! after the type.
+
+In viewDidLoad() we assign scene as promised, using the initializer we had just written moments ago. We tell it to fill the screen and then ask our view to present that scene to the user.
+*/
