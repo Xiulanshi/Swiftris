3050b80d10e778602aac0c158acf14f170b08a51
Xiulan Shi <xiulanshi2013@gmail.com>
2016-01-21 13:25:22 -0500

    First commit

---
+++ Swiftris/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Swiftris
+//
+//  Created by Xiulan Shi on 1/21/16.
+//  Copyright (c) 2016 Xiulan Shi. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+       
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ Swiftris/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameViewController.swift
+//  Swiftris
+//
+//  Created by Xiulan Shi on 1/21/16.
+//  Copyright (c) 2016 Xiulan Shi. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
